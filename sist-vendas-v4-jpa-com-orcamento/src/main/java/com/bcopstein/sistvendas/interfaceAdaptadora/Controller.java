bdbfbc79d65f1357c4b23d43c022f448507da84b
joaoWiskow <joao.marth@edu.pucrs.br>
2025-06-16 10:12:51 -0300

    Código original

---
+++ sist-vendas-v4-jpa-com-orcamento/src/main/java/com/bcopstein/sistvendas/interfaceAdaptadora/Controller.java
@@ -0,0 +1,70 @@
+package com.bcopstein.sistvendas.interfaceAdaptadora;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bcopstein.sistvendas.aplicacao.casosDeUso.BuscaOrcamentoUC;
+import com.bcopstein.sistvendas.aplicacao.casosDeUso.CriaOrcamentoUC;
+import com.bcopstein.sistvendas.aplicacao.casosDeUso.EfetivaOrcamentoUC;
+import com.bcopstein.sistvendas.aplicacao.casosDeUso.ProdutosDisponiveisUC;
+import com.bcopstein.sistvendas.aplicacao.dtos.ItemPedidoDTO;
+import com.bcopstein.sistvendas.aplicacao.dtos.OrcamentoDTO;
+import com.bcopstein.sistvendas.aplicacao.dtos.ProdutoDTO;
+import com.bcopstein.sistvendas.dominio.modelos.OrcamentoModel;
+
+
+@RestController
+public class Controller {
+    private ProdutosDisponiveisUC produtosDisponiveis;
+    private CriaOrcamentoUC criaOrcamento;
+    private EfetivaOrcamentoUC efetivaOrcamento;
+    private BuscaOrcamentoUC buscaOrcamento;
+
+    @Autowired
+    public Controller(ProdutosDisponiveisUC produtosDisponiveis,
+                      CriaOrcamentoUC criaOrcamento,
+                      EfetivaOrcamentoUC efetivaOrcamento,
+                      BuscaOrcamentoUC buscaOrcamento){
+        this.produtosDisponiveis = produtosDisponiveis;
+        this.criaOrcamento = criaOrcamento;
+        this.efetivaOrcamento = efetivaOrcamento;
+        this.buscaOrcamento = buscaOrcamento;
+    }
+
+    @GetMapping("")
+    @CrossOrigin(origins = "*")
+    public String welcomeMessage(){
+        return("Bem vindo as lojas ACME");
+    }
+
+    @GetMapping("produtosDisponiveis")
+    @CrossOrigin(origins = "*")
+    public List<ProdutoDTO> produtosDisponiveis(){
+        return produtosDisponiveis.run();
+    }    
+
+    @PostMapping("novoOrcamento")
+    @CrossOrigin(origins = "*")
+    public OrcamentoDTO novoOrcamento(@RequestBody List<ItemPedidoDTO> itens){
+        return criaOrcamento.run(itens);
+    }
+
+    @GetMapping("efetivaOrcamento/{id}")
+    @CrossOrigin(origins = "*")
+    public OrcamentoDTO efetivaOrcamento(@PathVariable(value="id") long idOrcamento){
+        return efetivaOrcamento.run(idOrcamento);
+    }
+
+    @GetMapping("buscaOrcamento/{id}")
+    @CrossOrigin(origins = "*")
+    public OrcamentoModel buscaOrcamento(@PathVariable(value="id") long idOrcamento){
+        return buscaOrcamento.run(idOrcamento);
+    }
+}
